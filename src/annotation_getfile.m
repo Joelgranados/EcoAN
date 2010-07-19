8f4f259001994c7e555cc01f6bc5bf8322fac608
Joel Andres Granados <joel.granados@gmail.com>
2010-07-19 13:08:37 +0200

    Added copyright to the files.

--- src/annotation_getfile.m
+++ src/annotation_getfile.m
@@ -1,3 +1,20 @@
+% Annotation.  An annotation creation tool for images.
+% Copyright (C) 2010 Joel Granados <joel.granados@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 
 % This function will gather all the getfile funcitonalities.
 function [file_path, success, ret_handles] =...
@@ -6,7 +23,7 @@ function [file_path, success, ret_handles] =...
     file_path = '';
     ret_handles = handles;
     temp_file = char(file_name);
-    
+
     % We check for ftp, ssh and local.
     if length(temp_file) > 6 && strcmp(temp_file(1:6), 'ftp://') == 1
         % then its an ftp file.
@@ -28,9 +45,9 @@ function [file_path, success, ret_handles] =...
     elseif length(temp_file) > 6 && strcmp(temp_file(1:6), 'ssh://') == 1
         [file_path, ap] = ssh_getfile(handles.ssh_struct,...
             temp_file(7:end), handles.config.cache_dir);
-        
+
         if ~file_path; return; end;
-        
+
         % Change the state
         handles.curr_ann.ftp = 0;
         handles.curr_ann.ssh = 1;
