20184f86dc2a71b45b28972a6bcec95ffc1b6d6b
Joel Andres Granados <joel.granados@gmail.com>
2015-04-08 19:11:36 +0200

    Fix plotSignal function
    * plotSignal.m (plotSignal): Add an argument to specify the column.

--- plotSignal.m
+++ plotSignal.m
@@ -1,13 +1,13 @@
-function signal = plotSignal(filename)
+function signal = plotSignal(filename, col)
 
     fd = fopen(filename);
-    data = textscan(fd, '%s %s', 'delimiter', ',', 'Headerlines', 2);
+    data = textscan(fd, '%s %s %s %s %s %s', 'delimiter', ',', 'Headerlines', 3);
     fclose(fd);
 
     signal = [];
     for i=1:size(data{1},1),
         signal(i,1) = datenum(data{1}(i), 'yyyy-mm-dd');
-        signal(i,2) = str2num(data{2}{i});
+        signal(i,2) = str2num(data{col}{i});
     end
 
     plot(signal(:,1), signal(:,2));
